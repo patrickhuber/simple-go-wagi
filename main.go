ba5f1965fde634945bf3f0d12eeb4162063b52f5
Patrick Huber <182398+patrickhuber@users.noreply.github.com>
2022-06-05 10:07:16 -0400

    dumps environment variables

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
+import "os"
+
 func main() {
 	println("content-type: text/plain;utf-8")
 	println("")
-	println("Hello World!")
+	for _, e := range os.Environ() {
+		println(e)
+	}
 }
